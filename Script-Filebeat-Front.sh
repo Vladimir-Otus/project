cb28cc9c03d398bc9d3cf8480d3f02a5f5d74010
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-09 23:19:01 +0300

    Update Script-Filebeat-Front.sh

--- Script-Filebeat-Front.sh
+++ Script-Filebeat-Front.sh
@@ -1,12 +1,35 @@
-#установка filebeat на Front
-dpkg -i *.deb
+# Установка Filebeat из .deb пакета
+sudo dpkg -i *.deb
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось установить Filebeat."
+    exit 1
+fi
 
-# скачать  конфигурацию для filebeat Front
-wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/filebeat-Front.yml
+# Скачать конфигурацию для Filebeat Front
+wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/filebeat-Front.yml -O /tmp/filebeat-Front.yml
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось скачать конфигурацию."
+    exit 1
+fi
 
-# заменить  
-cp filebeat-Front.yml /etc/filebeat/filebeat.yml
+# Заменить конфигурацию Filebeat
+sudo cp -f /tmp/filebeat-Front.yml /etc/filebeat/filebeat.yml
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось скопировать конфигурацию."
+    exit 1
+fi
 
-# запустить
-systemctl restart filebeat
-systemctl enable filebeat
+# Перезапустить и включить Filebeat
+sudo systemctl restart filebeat
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось перезапустить Filebeat."
+    exit 1
+fi
+
+sudo systemctl enable filebeat
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось включить автозапуск Filebeat."
+    exit 1
+fi
+
+echo "Filebeat успешно установлен и настроен."
