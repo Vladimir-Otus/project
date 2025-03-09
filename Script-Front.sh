a2b0ab83264914724cdda55be3e04436a4cc1c78
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-09 21:31:35 +0300

    Update Script-Front.sh

--- Script-Front.sh
+++ Script-Front.sh
@@ -2,7 +2,7 @@
 apt install nginx -y;
 
 # Скачать nginx site-enable default и переместить
-wget https://github.com/Vladimir-Otus/project/blob/ced4b94fc1f674bb108894f88c453295c1fe23d6/nginx-sites-available
+wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/nginx-sites-available
 cp nginx-sites-available /etc/nginx/sites-available/default;
 
 # рестарт nginx
