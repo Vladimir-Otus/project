5fb0eb29c598cd8fccf7b904355c28dbfd2c9e84
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-09 23:23:47 +0300

    Update Script-Filebeat-Front.sh

--- Script-Filebeat-Front.sh
+++ Script-Filebeat-Front.sh
@@ -1,9 +1,22 @@
-# Установка Filebeat из .deb пакета
-sudo dpkg -i *.deb
-if [ $? -ne 0 ]; then
-    echo "Ошибка: не удалось установить Filebeat."
+# Проверка наличия файла .deb
+if [ ! -f /home/vt/filebeat-8.9.1-amd64.deb ]; then
+    echo "Ошибка: файл /home/vt/filebeat-8.9.1-amd64.deb не найден."
     exit 1
 fi
+
+# Установка Filebeat из .deb пакета
+sudo dpkg -i /home/vt/filebeat-8.9.1-amd64.deb
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось установить Filebeat."
+    echo "Попытка установить зависимости..."
+    sudo apt-get install -f -y
+    if [ $? -ne 0 ]; then
+        echo "Ошибка: не удалось установить зависимости."
+        exit 1
+    fi
+fi
+
+echo "Filebeat успешно установлен."
 
 # Скачать конфигурацию для Filebeat Front
 wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/filebeat-Front.yml -O /tmp/filebeat-Front.yml
