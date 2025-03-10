72a3db4c1e2723fc3003df3c0954ea0b9b1fdb18
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-10 11:04:38 +0300

    Update Back2-SlaveBD.sh

--- Back2-SlaveBD.sh
+++ Back2-SlaveBD.sh
@@ -1,6 +1,6 @@
 # Установка MySQL 8.0
 sudo apt update
-sudo apt install mysql-server-8.0 -y
+sudo apt install mysql-server -y
 
 # Загрузка и замена конфигурационного файла
 sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/Slave-mysql.cnf -O /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -8,11 +8,14 @@ sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/mai
 # Перезапуск MySQL
 sudo service mysql restart
 
-### на всякий случай останавливаем текущую репликацию
+# Остановка текущей репликации (если она была настроена)
 mysql -u root -e "STOP REPLICA;"
 
-### Устанавливаем мастер для реплики
+# Настройка мастера для репликации
 mysql -u root -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='172.16.10.110', SOURCE_USER='repl', SOURCE_PASSWORD='oTUSlave#2020', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;"
 
-### Стартуем реплику
+# Запуск репликации
 mysql -u root -e "START REPLICA;"
+
+# Проверка статуса репликации
+mysql -u root -e "SHOW REPLICA STATUS\G;"
