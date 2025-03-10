38339381b5ba2773aefcf33524744f2eb48f54e9
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-10 09:56:25 +0300

    Update back1-apache.sh

--- back1-apache.sh
+++ back1-apache.sh
@@ -1,5 +1,5 @@
 # Установка Apache2
-apt install apache2 -y
+sudo apt install apache2 -y
 
 # Заменить "Apache2 Default Page" на "Apache2 - Back1" в файле index.html
-sed -i 's/Apache2 Default Page/Apache2 - Back1/' /var/www/html/index.html
+sudo sed -i 's/Apache2 Default Page/Apache2 - Back1/' /var/www/html/index.html
