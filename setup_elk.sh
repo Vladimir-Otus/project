4ea067bf451904759635b82f705178f017dd1046
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-10 14:48:01 +0300

    Update setup_elk.sh

--- setup_elk.sh
+++ setup_elk.sh
@@ -1,36 +1,36 @@
-### закинул deb пакеты в home
-### меняем hostname для конфига elasticsearch надо
+# закинул deb пакеты в home
+# меняем hostname для конфига elasticsearch надо
 sudo hostnamectl set-hostname ELK-Grafana
 sudo sed -i "s/^127.0.1.1 .*/127.0.1.1 ELK-Grafana/" /etc/hosts
-echo "✅ Hostname изменен на: $(hostname)"
+echo "Hostname изменен на: $(hostname)"
 
-### для работы нужен jdk
+# для работы нужен jdk
 sudo apt install default-jdk -y
 
-### устанавливаем пакеты пачкой из папки ELK
-###cd ELK
+# устанавливаем пакеты пачкой из папки ELK
+#cd ELK
 sudo dpkg -i *.deb
 sudo apt-get install -f -y  # Установка недостающих зависимостей
 
-### устанавливаем лимиты для оперативки
+# устанавливаем лимиты для оперативки
 sudo mkdir -p /etc/elasticsearch/jvm.options.d
 echo -e "-Xms1g\n-Xmx1g" | sudo tee /etc/elasticsearch/jvm.options.d/jvm.options > /dev/null
 
-### скачиваем конфиги
-wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/elasticsearch.yml
-wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/filebeat.yml
-wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/kibana.yml
-wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/logstash.yml
-wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/logstash-nginx-es.conf
+# скачиваем конфиги
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/elasticsearch.yml
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/filebeat.yml
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/kibana.yml
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/logstash.yml
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/logstash-nginx.conf
 
-### копируем в нужные папки
+# копируем в нужные папки
 sudo cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 sudo cp filebeat.yml /etc/filebeat/filebeat.yml
 sudo cp kibana.yml /etc/kibana/kibana.yml
 sudo cp logstash.yml /etc/logstash/logstash.yml
 sudo cp logstash-nginx-es.conf /etc/logstash/conf.d/logstash-nginx-es.conf
 
-### рестартуем все возможное
+# рестартуем все возможное
 sudo systemctl daemon-reload
 sudo systemctl enable --now elasticsearch.service
 sudo systemctl enable --now kibana.service
