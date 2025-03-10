c4262b6f307a5ba85f223bae176232a529f6f736
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-10 10:58:41 +0300

    Update CMS.sh

--- CMS.sh
+++ CMS.sh
@@ -2,11 +2,11 @@
 sudo apt install php libapache2-mod-php php-mysql php-curl php-gd php-json php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y;
 
 ### скачать wordpress, распаковать
-wget https://ru.wordpress.org/wordpress-6.7.1-ru_RU.tar.gz;
-tar -xzvf wordpress-6.7.1-ru_RU.tar.gz;
+sudo wget https://ru.wordpress.org/wordpress-6.7.1-ru_RU.tar.gz;
+sudo tar -xzvf wordpress-6.7.1-ru_RU.tar.gz;
 
 ### копируем все в папку apache
-cp -r wordpress/* /var/www/html/;
+sudo cp -r wordpress/* /var/www/html/;
 
 ### создаем базу для wp
 mysql -u root -e "CREATE DATABASE WP;";
@@ -16,10 +16,10 @@ mysql -u root -e "CREATE USER 'wp'@'%' IDENTIFIED BY 'password';";
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'wp'@'%';";
 
 ### качаем wp-config
-wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/wp-config.php
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/wp.php
 
 ### копируем его в папку wordpress
-cp wp-config.php /var/www/html/;
+sudo cp wp-config.php /var/www/html/;
 
 ### удаляем файл apache index (чтобы резолвился нормальный wordpress)
 sudo rm /var/www/html/index.html;
