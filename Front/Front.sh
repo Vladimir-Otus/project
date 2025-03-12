3837125038f077ccb09e210ee9f0475a96fae6ae
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-12 14:39:01 +0300

    Add files via upload

---
+++ Front/Front.sh
@@ -0,0 +1,30 @@
+# Установка Nginx
+sudo apt install nginx -y;
+
+# Скачать nginx site-enable default
+sudo wget https://raw.githubusercontent.com/Vladimir-Otus/project/refs/heads/main/nginx-sites-available -O /tmp/nginx-sites-available
+
+# Проверить, скачался ли файл
+if [ -f /tmp/nginx-sites-available ]; then
+    echo "Файл успешно скачан."
+
+    # Копировать файл в /etc/nginx/sites-available/default
+    sudo cp -f /tmp/nginx-sites-available /etc/nginx/sites-available/default
+
+    # Проверить, скопировался ли файл
+    if [ -f /etc/nginx/sites-available/default ]; then
+        echo "Файл успешно скопирован в /etc/nginx/sites-available/default."
+    else
+        echo "Ошибка: файл не скопирован. Проверьте права доступа или путь."
+        exit 1
+    fi
+else
+    echo "Ошибка: файл не скачан. Проверьте URL или подключение к интернету."
+    exit 1
+fi
+
+# Перезапуск Nginx
+sudo systemctl restart nginx;
+
+# Установка Prometheus
+sudo apt install prometheus -y;
